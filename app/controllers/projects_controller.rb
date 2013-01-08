f2a1d010aae42e77a0377f9da11678043ebf2e8a
varunrau <varunrau@gmail.com>
2013-01-08 14:32:56 -0500

    added projects into database

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,8 +19,19 @@ class ProjectsController < ApplicationController
     @project = Project.new
 
     respond_to do |format|
-      format.html
+      format.html # new.html.erb
       format.json { render json: @login }
+    end
+  end
+
+  def create
+    @project = Project.new(params[:project])
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to @project, notice: 'Project created' }
+      else
+        format.html { render action: "new" }
+      end
     end
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,3 @@
 class Project < ActiveRecord::Base
-  attr_accessible :description, :image, :name
+  attr_accessible :description, :image, :tech, :name
 end
